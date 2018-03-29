cb89b33c546c63aa946b7bfff6e55a21f8185978
LOI DINH <loiphatdinh@gmail.com>
2018-03-29 23:57:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function getFirstSelector(selector) {
+  var elements = document.querySelector(selector);
+  return elements;
+}
+
+function nestedTarget() {
+
+  return document.querySelector('#nested .target');
+
+}
